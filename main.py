55e2f9480e4a3659e52f116f0a1a5d7692b73b95
tshavit2011-bit <tshavit2011@gmail.com>
2026-01-07 08:15:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-# Download the data from GitHub
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
-
-import lab_setup_do_not_edit
 import pandas as pd
 
-# Load the dataset
 df = pd.read_csv("/content/parkinsons.csv")
+df = df.dropna()
 
-# Select features and label
-selected_features = ["PPE", "MDVP:Fo(Hz)"]
+print(df.columns.to_list())
+
+selected_features = ['RPDE', 'PPE']
 X = df[selected_features]
-y = df["status"]
+y = df['status']
 
-# Scale features
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
-# Split data into train and test sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Train KNN model
-from sklearn.neighbors import KNeighborsClassifier
-model = KNeighborsClassifier(n_neighbors=10)
-model.fit(X_train, y_train)
+from sklearn.tree import DecisionTreeClassifier
+DTC = DecisionTreeClassifier(max_depth = 3)
+DTC.fit(X_train, y_train)
 
-# Evaluate the model
 from sklearn.metrics import accuracy_score
-y_pred = model.predict(X_test)
+y_pred = DTC.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-
-print(f"Accuracy: {accuracy}")
+print(accuracy)
